33ab83e081c80ec21da8f59d1eca950710b0c042
Dreeeems <m.alexandre.delpy@gmail.com>
2025-05-13 14:01:17 +0200

    Timeline adding

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import DinoTimeline from "./components/dinoTimeline";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className=""></div>
-    </>
+    <main className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 text-white">
+      <div className="container mx-auto px-4 py-10">
+        <h1 className="text-4xl md:text-5xl font-bold text-center mb-8">
+          Chronosauria
+        </h1>
+        <p className="text-lg max-w-2xl text-center mx-auto mb-12 text-slate-300">
+          Journey through time and interact with 3D dinosaurs from different
+          periods of the Mesozoic Era
+        </p>
+        <DinoTimeline />
+      </div>
+    </main>
   );
 }
 
