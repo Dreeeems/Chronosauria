d4ea60721763f3fb35d4c4562fec3f5accd48f1a
Dreeeems <m.alexandre.delpy@gmail.com>
2025-05-13 14:22:34 +0200

    building the timeline

--- src/components/dinoTimeline.jsx
+++ src/components/dinoTimeline.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import { React, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { ChevronLeft, ChevronRight, Info } from "lucide-react";
+import { DinosaursData } from "../data/dinoData";
 
 const DinoTimeline = () => {
+  const [activePeriod, setactivePeriod] = useState("triassic");
+  const [selectedDinosaur, setSelectedDinosaur] = useState(null);
+  const periods = [
+    {
+      id: "triassic",
+      name: "Triassic",
+      years: "252 to 201 MYA",
+      color: "",
+    },
+    {
+      id: "jurassic ",
+      name: "Jurassic ",
+      years: "201 to 145 MYA",
+      color: "",
+    },
+    {
+      id: "cretaceous ",
+      name: "Cretaceous ",
+      years: "145 to 66 MYA",
+      color: "",
+    },
+  ];
+
+  const activeDinosaurs = DinosaursData.filter(
+    (dino) => dino.period == activePeriod
+  );
   return <div>Timeline</div>;
 };
 
